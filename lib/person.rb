af9b9da371d61b47a48efa48c858139dde805c27
Jenny Chu <jchudesign0930@gmail.com>
2019-04-08 22:11:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,11 +3,10 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
  
-  def initialize(user_name:, name:, age:, location:)
-    @user_name = user_name
-    @name = name
-    @location = location
-    @age = age
+ 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
+
 end
 end
